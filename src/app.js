ed2ea9871d2c1aad681f40779db49f3ec812da45
Jon Hall <jon@flowmo.co>
2016-12-04 17:29:49 +0000

    Set up graphql api via express

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
-import { graphql, buildSchema } from 'graphql';
+import { graphql } from 'graphql';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
 import schema from './schema';
 
-let id = 1;
-let query = `{
-    todo(id: ${id}) {
-        id,
-        text,
-        complete
-    }
-}`;
+let app = express();
 
-graphql(schema, query).then((response) => {
-  console.log(response);
-});
+app.use('/graphiql', graphqlHTTP({
+  schema: schema,
+  rootValue: {},
+  graphiql: true,
+}));
+
+app.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: {}
+}));
+
+app.listen(4000);
+
+console.log('Running a GraphQL API server at localhost:4000');
+console.log('GraphiQL can be accessed at localhost:4000/graphiql');
